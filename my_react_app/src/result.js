07df0fbc98a04728d0c7ff1cd9a8f6104cc3d7d2
Tianmu Lan <tlan4@cs.brown.edu>
2019-03-09 18:03:21 -0500

    multi page

---
+++ my_react_app/src/result.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Result extends Component {
+	render() {
+		return (
+			<div className="Result">
+				This is result Page!
+			</div>
+		);
+	}
+}
+
+export default Result;
---
+++ my_react_app/src/search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+	render() {
+		return (
+			<div className="Search">
+				This is search Page!
+			</div>
+		);
+	}
+}
+
+export default Search;
