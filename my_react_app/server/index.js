2ee1f9da5bafe016b111800ffbf92a040641b75d
Tianmu Lan <tianmu@umich.edu>
2019-04-10 02:03:19 -0400

    web1 works

--- my_react_app/server/index.js
+++ my_react_app/server/index.js
@@ -1,18 +1,28 @@
 const axios = require('axios')
 const express = require('express');
-const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
-
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(pino);
 
-let project1 = "";
+let project1 = {};
 let project2 = "";
-axios.get('https://library.brown.edu/iip_development/mapsearch/').then((response) => {
-	project1 = response.data.toString();
-	console.log("Project 1 cached!");
-});
+
+const project1_facets_lists = ["type", "city", "language_display", "physical_type", "religion", "material"];
+for( i = 0; i < project1_facets_lists.length; i++){
+	let facet = project1_facets_lists[i];
+	axios.get('https://library.brown.edu/search/solr_pub/iip/?q=*:*&start=0&rows=0&indent=on&facet=on&facet.field=' + facet + '&wt=json').then((response) => {
+		let returnData = response.data.facet_counts.facet_fields[facet];
+		let facetList = [];
+		for ( j = 0; j < returnData.length; j++){
+			if( j%2 == 0 && returnData[j]!="") facetList.push(returnData[j]);			
+		}
+		project1[facet] = facetList;
+		console.log(facet + " Get!");
+	});
+}
 
 axios.get('https://edh-www.adw.uni-heidelberg.de/inschrift/erweiterteSuche').then((response) => {		
 	project2 = response.data.toString();
@@ -20,14 +30,25 @@ axios.get('https://edh-www.adw.uni-heidelberg.de/inschrift/erweiterteSuche').the
 });
 
 app.get('/search1', (req, res) => {
-	res.setHeader('Content-Type', 'text/plain');
-	res.send( project1 );
+	res.setHeader('Content-Type', 'application/json');
+	res.end( JSON.stringify(project1) );
 });
 app.get('/search2', (req, res) => {
 	res.setHeader('Content-Type', 'text/plain');
 	res.send( project2 );
 });
+app.post('/query1', (req, res) => {
+	let website = 'https://library.brown.edu/search/solr_pub/iip/?';
+	let showWhich = 'start=' + req.body.start + '&rows=' + req.body.rows;
+	let urlPostfix = '&indent=on&wt=json&q=';
+	console.log(website + showWhich + urlPostfix + req.body.queryStr);
 
+	axios.get(website + showWhich + urlPostfix + req.body.queryStr).then((response) => {		
+		res.setHeader('Content-Type', 'application/json');
+		res.end( JSON.stringify(response.data) );		
+		console.log("query 1 result returned!");
+	});
+});
 //app.get('/details', (req, res) => {
 //    axios.get('https://library.brown.edu/search/solr_pub/iip/?start=0&rows=1&indent=on&wt=json&q=*').then((response) => {
 //        console.log("fetched");
--- my_react_app/src/search.js
+++ my_react_app/src/search.js
@@ -6,10 +6,20 @@ const capitalize = (s) => {
 	return s.charAt(0).toUpperCase() + s.slice(1)
 }
 
+const project1Mapping = {
+	locationsSel: 'region',
+	typesSel: 'type',
+	physicalTypesSel: 'physical_type',
+	languagesSel: 'language',
+	religionsSel: 'religion',
+	materialsSel: 'material',
+}
+
 class Search extends Component {
 	constructor(props) {
 		super(props);
-		this.fetchData = this.fetchData.bind(this);
+		this.fetchData = this.fetchData.bind(this);		
+		this.handleSubmit = this.handleSubmit.bind(this);
 		this.state = {
 				locations: [],
 				types:[],
@@ -17,6 +27,13 @@ class Search extends Component {
 				languages:[],
 				religions:[],
 				materials:[],
+				/*Selected Fields*/
+				locationsSel: [],
+				typesSel:[],
+				physicalTypesSel:[],
+				languagesSel:[],
+				religionsSel:[],
+				materialsSel:[],
 			};
 	}
 
@@ -25,8 +42,17 @@ class Search extends Component {
 	}
 
 	fetchData(){
-	    fetch(`/search1`).then(response => response.text()).then(state => {	
-			let parser = new DOMParser();
+	    fetch(`/search1`).then(response => response.json()).then(data => {	
+			this.setState({
+				locations:this.uniq(this.state.locations.concat(data.city)),
+				types:this.uniq(this.state.types.concat(data.type)),
+				physicalTypes:this.uniq(this.state.physicalTypes.concat(data.physical_type)),
+				languages:this.uniq(this.state.languages.concat(data.language_display)),
+				religions:this.uniq(this.state.religions.concat(data.religion)),
+				materials:this.uniq(this.state.materials.concat(data.material)),
+			});
+
+			/*let parser = new DOMParser();
 			let doc = parser.parseFromString(state,"text/html");
 			this.setState({
 				locations:this.uniq(this.state.locations.concat(this.findData1(doc, "id_place_"))),
@@ -36,10 +62,10 @@ class Search extends Component {
 				religions:this.uniq(this.state.religions.concat(this.findData1(doc, "id_religion_"))),
 				materials:this.uniq(this.state.materials.concat(this.findData1(doc, "id_material_"))),
 
-			});
+			});*/
 		});
 
-		fetch(`/search2`).then(response => response.text()).then(state => {	
+		/*fetch(`/search2`).then(response => response.text()).then(state => {	
 			let parser = new DOMParser();
 			let doc = parser.parseFromString(state,"text/html");
 			
@@ -53,11 +79,11 @@ class Search extends Component {
 
 			});
 			
-		});
+		});*/
 	}
 
 	//For first website
-	findData1(doc, idString){
+	/*findData1(doc, idString){
 		let list = [];
 		for (var i = 0; ;i++){
 			let temp = doc.getElementById(idString+i); 
@@ -65,7 +91,7 @@ class Search extends Component {
 			else break;
 		}
 		return list;		
-	}
+	}*/
 
 	//For second website
 	findData2(doc, nameString){
@@ -101,13 +127,13 @@ class Search extends Component {
 	render() {
 		return (
 			<div className="Search">
-				<form action="./result" method="get">
-					<Filter name="Location" data={this.state.locations}/>
-					<Filter name="Type of Inscription" data={this.state.types}/>
-					<Filter name="Physical Type" data={this.state.physicalTypes}/>
-					<Filter name="Language" data={this.state.languages}/>
-					<Filter name="Religion" data={this.state.religions}/>
-					<Filter name="Material" data={this.state.materials}/>
+				<form action="./result" onSubmit={this.handleSubmit}>
+					<Filter name="Location" data={this.state.locations} parent={this} cat="locations"/>
+					<Filter name="Type of Inscription" data={this.state.types} parent={this} cat="types"/>
+					<Filter name="Physical Type" data={this.state.physicalTypes} parent={this} cat="physicalTypes"/>
+					<Filter name="Language" data={this.state.languages} parent={this} cat="languages"/>
+					<Filter name="Religion" data={this.state.religions} parent={this} cat="religions"/>
+					<Filter name="Material" data={this.state.materials} parent={this} cat="materials"/>
 					<div style={{width:"800px", textAlign:"center"}}>
 					<br /> 
 						<div>From: <input type="number" id="from" name="from" step="1"/></div>
@@ -119,6 +145,46 @@ class Search extends Component {
 			</div>
 		);
 	}
+
+	handleSubmit(event){
+		event.preventDefault();
+		console.log(this.state)	
+		/*https://library.brown.edu/search/solr_pub/iip/?start=0&rows=100&indent=on&wt=json&q=region:%22Coastal+Plain%22+%22AND%22+religion:%22jewish%22*/	
+		let urlPrefix = 'https://library.brown.edu/search/solr_pub/iip/?';
+		let urlChange = 'start=0&rows=100';
+		let urlPostfix = '&indent=on&wt=json&q=';
+		let query = '';
+		for (let objname in project1Mapping){
+			if (this.state[objname].length !== 0){
+				if ( query === '' ) query = query + project1Mapping[objname] + ':';
+				else query = query + '+AND+' + project1Mapping[objname] + ':';
+
+				if(this.state[objname].length ===1) query=query+'%22' + encodeURI(this.state[objname][0].split(' ').join('+')) +'%22';
+				else{
+					for( let i = 0; i < this.state[objname].length; i++){
+						if(i===0) query = query+'(';
+						else query = query+'+OR+';
+						query = query + '%22' + encodeURI(this.state[objname][i].split(' ').join('+')) + '%22';
+					}
+					query = query+')';
+				}
+
+			}
+		}
+		if (query === '') query = '*:*';
+		console.log(query)
+		let jsonData = { queryStr:query, start:"0", rows:"100" };
+		fetch("/query1", {
+			method: "post",
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(jsonData),
+		}).then(response => response.json()).then(data => {console.log(data)});		
+
+
+		/*https://edh-www.adw.uni-heidelberg.de/inschrift/erweiterteSuche?hd_nr=&tm_nr=&land=&fo_antik=&fo_modern=&fundstelle=&region=&compFundjahr=eq&fundjahr=&aufbewahrung=&inschriftgattung=&sprache=It&inschrifttraeger=&compHoehe=eq&hoehe=&compBreite=eq&breite=&compTiefe=eq&tiefe=&bh=&palSchreibtechnik=&dat_tag=&dat_monat=&dat_jahr_a=&dat_jahr_e=&hist_periode=&religion=&literatur=&kommentar=&p_name=&p_praenomen=&p_nomen=&p_cognomen=&p_supernomen=&p_tribus=&p_origo=&p_geschlecht=&p_status=&compJahre=eq&p_lJahre=&compMonate=eq&p_lMonate=&compTage=eq&p_lTage=&compStunden=eq&p_lStunden=&atext1=&bool=AND&atext2=&sort=hd_nr&anzahl=20*/
+	};
 }
 
 class Filter extends Component {
@@ -129,7 +195,7 @@ class Filter extends Component {
 			<span>{this.props.name}</span>
 				<div className="Filter">			
 				{this.props.data.map((name, idx) => (
-					<CheckBoxSample name={name} id={name} value={name} key={idx}/>
+					<CheckBoxSample name={name} id={this.props.name+name} key={idx} pp={this.props.parent} pcat={this.props.cat}/>
 				))}
 				</div>
 			</div>
@@ -138,13 +204,34 @@ class Filter extends Component {
 }
 
 class CheckBoxSample extends Component {
+	constructor(props) {
+		super(props);		
+		this.handleChange = this.handleChange.bind(this);
+	}
 	render() {
 		return (
 			<div className="CheckBoxSample">
-			<input name={this.props.name} value={this.props.value} id={this.props.name} type="checkbox"/>
+			<input name={this.props.name} id={this.props.id+"Checkbox"} type="checkbox" onChange={this.handleChange}/>
 			<span>{'  '}{this.props.name}</span>
 			</div>
 		);
+	}
+	handleChange(event){
+		if(event.target.checked) {
+			let catStr = this.props.pcat + 'Sel';
+			let prev = this.props.pp.state[catStr];
+			prev.push(this.props.name)
+			this.props.pp.setState({ [catStr]: prev })
+		}
+		else{
+			let catStr = this.props.pcat + 'Sel';
+			let prev = this.props.pp.state[catStr];
+			let idx = prev.indexOf(this.props.name);
+			if (idx > -1) {
+			  prev.splice(idx, 1);
+			}
+			this.props.pp.setState({ [catStr]: prev });			
+		}
 	}
 }
 export default Search;
