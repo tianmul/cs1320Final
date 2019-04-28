781cba8cb5bea4652470093cfb56534f7dc6e40f
Ubuntu <ubuntu@ip-172-31-47-163.us-east-2.compute.internal>
2019-04-28 21:32:10 +0000

    cors fix

--- my_react_app/server/index.js
+++ my_react_app/server/index.js
@@ -4,7 +4,7 @@ const pino = require('express-pino-logger')();
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser')
-
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 //app.use(pino);
@@ -15,7 +15,6 @@ let project2 = "";
 const project1_facets_lists = ["type", "city", "language_display", "physical_type", "religion", "material"];
 for( i = 0; i < project1_facets_lists.length; i++){
 	let facet = project1_facets_lists[i];
-	console.log("getting");
 	axios.get('https://library.brown.edu/search/solr_pub/iip/?q=*:*&start=0&rows=0&indent=on&facet=on&facet.field=' + facet + '&wt=json').then((response) => {
 		let returnData = response.data.facet_counts.facet_fields[facet];
 		let facetList = [];
@@ -41,7 +40,7 @@ app.get('/search1', (req, res) => {
 	res.setHeader('Content-Type', 'application/json');
 	res.end( JSON.stringify(project1) );
 });
-app.get('/search2', cors(), (req, res) => {
+app.get('/search2', (req, res) => {
 	res.setHeader('Content-Type', 'text/plain');
 	res.send( project2 );
 });
